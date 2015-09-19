85c1d6200e168e9b494f86a64223ec286c6ad5bd
robgoodfellow <otherness-space@users.noreply.github.com>
2015-09-19 19:03:50 -0400

    added to index.php to test Angular is working. Also, testing for nobase error

--- index.php
+++ index.php
@@ -2,11 +2,27 @@
 <!DOCTYPE html>
 <html ng-app>
 <head>
+    <!-- <base href="/jsonapi/"> -->
     <title>AngularJS Basic Theme Name</title>
     <?php wp_head(); ?>
 </head>
 
 <body>
+    <header>
+		<h1>
+			<a href="<?php echo site_url(); ?>">AngularJS Demo Theme</a>
+		</h1>
+	</header>
+
+	<div>
+		<input type="text" ng-model="name">
+
+		<p>Hello, {{name}}!</p>
+	</div>
+
+	<footer>
+		&copy; <?php echo date( 'Y' ); ?>
+	</footer>
 </body>
 
 </html>
