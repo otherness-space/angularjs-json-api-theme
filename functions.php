0bd697731fec2af22c64a20f0956ca3e9ab75a4e
robgoodfellow <otherness-space@users.noreply.github.com>
2015-09-19 18:35:17 -0400

    installed functions.php to enqueue scripts

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function my_scripts() {
+
+	wp_enqueue_script(
+		'angularjs',
+		get_stylesheet_directory_uri() . '/bower_components/angular/angular.min.js'
+	);
+	wp_enqueue_script(
+		'angularjs-route',
+		get_stylesheet_directory_uri() . '/bower_components/angular-route/angular-route.min.js'
+	);
+}
+add_action( 'wp_enqueue_scripts', 'my_scripts' );
