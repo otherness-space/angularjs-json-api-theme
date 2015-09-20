758b7f0bf6e9ffdf8efb601215ff411fa4af0809
robgoodfellow <otherness-space@users.noreply.github.com>
2015-09-19 22:54:17 -0400

    edit functions.php

--- functions.php
+++ functions.php
@@ -1,22 +1,28 @@
 <?php
+
 function my_scripts() {
+
 	wp_register_script(
 		'angularjs',
 		get_stylesheet_directory_uri() . '/bower_components/angular/angular.min.js'
 	);
+
 	wp_register_script(
 		'angularjs-route',
 		get_stylesheet_directory_uri() . '/bower_components/angular-route/angular-route.min.js'
 	);
+
 	wp_register_script(
 		'angularjs-sanitize',
 		get_stylesheet_directory_uri() . '/bower_components/angular-sanitize/angular-sanitize.min.js'
 	);
+
 	wp_enqueue_script(
 		'my-scripts',
 		get_stylesheet_directory_uri() . '/js/scripts.js',
 		array( 'angularjs', 'angularjs-route', 'angularjs-sanitize' )
 	);
+
 	wp_localize_script(
 		'my-scripts',
 		'myLocalized',
@@ -25,4 +31,5 @@ function my_scripts() {
 			)
 	);
 }
+
 add_action( 'wp_enqueue_scripts', 'my_scripts' );
