bde2f2f1273552e05cda45dd9afa2386b161eb99
robgoodfellow <otherness-space@users.noreply.github.com>
2015-09-19 20:13:56 -0400

    update the functions.php, enqueue scripts.js, and use wp_localized_script to make the URL of the partials folder available to scripts.js

--- functions.php
+++ functions.php
@@ -9,5 +9,17 @@ function my_scripts() {
 		'angularjs-route',
 		get_stylesheet_directory_uri() . '/bower_components/angular-route/angular-route.min.js'
 	);
+	wp_enqueue_script(
+		'my-scripts',
+		get_stylesheet_directory_uri() . '/js/scripts.js',
+		array( 'angularjs', 'angularjs-route' )
+	);
+	wp_localize_script(
+		'my-scripts',
+		'myLocalized',
+		array(
+			'partials' => trailingslashit( get_template_directory_uri() ) . 'partials/'
+			)
+	);
 }
 add_action( 'wp_enqueue_scripts', 'my_scripts' );
