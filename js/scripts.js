3055edbe4d705b88636f3dd8281b56dde9fd1111
robgoodfellow <otherness-space@users.noreply.github.com>
2015-09-19 22:02:36 -0400

    edit script.js and add /partials/content.html

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,19 @@ angular.module('app', ['ngRoute'])
 	.when('/', {
 		templateUrl: myLocalized.partials + 'main.html',
 		controller: 'Main'
+	})
+	.when('/:ID', {
+		templateUrl: myLocalized.partials + 'content.html',
+		controller: 'Content'
 	});
 })
 .controller('Main', function($scope, $http, $routeParams) {
 	$http.get('wp-json/posts/').success(function(res){
 		$scope.posts = res;
+	});
+})
+.controller('Content', function($scope, $http, $routeParams) {
+	$http.get('wp-json/posts/' + $routeParams.ID).success(function(res){
+		$scope.post = res;
 	});
 });
