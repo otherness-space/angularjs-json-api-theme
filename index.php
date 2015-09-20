9e22755e42d3abc542cb5b5e48264860847d647d
robgoodfellow <otherness-space@users.noreply.github.com>
2015-09-19 20:56:34 -0400

    rewrote base href

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html ng-app="app">
 <head>
-    <base href="/jsonapi/">
+    <base href="<?php $url_info = parse_url( home_url() ); echo trailingslashit( $url_info['path'] ); ?>">
     <title>AngularJS Basic Theme Name</title>
     <?php wp_head(); ?>
 </head>
